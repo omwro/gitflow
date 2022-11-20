77365801b63efc1bc8b3cf6bf8ac2f6c232b0523
Omer Erdem <m.o.erdem@outlook.com>
2022-11-20 13:37:56 +0100

    '1.1.1'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
+import App from '@/App'
 import GitFlow from "@/GitFlow";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: function (h) { return h(GitFlow) },
+  render: function (h) { return h(App) },
 }).$mount('#app')
